a1fd549d5e838908e8398f15e130e8ac6b77590f
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-05-15 12:25:45 +0200

    Add styles to search container, input and search button

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -65,4 +65,31 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
   },
+  searchContainer: {
+    marginTop: 30,
+    height: '7%',
+    marginHorizontal: 16,
+    position: 'relative',
+    zIndex: 50,
+  },
+  searchBox: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  input: {
+    paddingLeft: 24,
+    height: 40,
+    paddingBottom: 4,
+    flex: 1,
+    fontSize: 16,
+    color: 'white',
+  },
+  searchButton: {
+    backgroundColor: 'rgb(255, 255, 255)',
+    borderRadius: 50,
+    padding: 12,
+    margin: 4,
+  },
 });
