534a366dd55a0c41cd256c20f49b3e3cb6d3fb82
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-05-15 10:39:51 +0200

    Add component FirstScreen with background image and SafeArea

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -1,1 +1,18 @@
-export default function FirstScreen() {}
+import { View, SafeAreaView, Image } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
+
+export default function FirstScreen() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style='light' />
+      <Image
+        source={require('../assets/images/tlo.jpg')}
+        style={styles.imageBackground}
+      />
+      <SafeAreaView style={styles.safeArea}></SafeAreaView>
+    </View>
+  );
+}
+
