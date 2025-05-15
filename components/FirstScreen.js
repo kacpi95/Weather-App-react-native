758c704c53f789b5c65a97a197478a3e68f8e3c3
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-05-15 11:59:54 +0200

    Add search container with Textinput and MagnifyGlassIcon

--- components/FirstScreen.js
+++ components/FirstScreen.js
@@ -1,9 +1,18 @@
-import { View, SafeAreaView, Image } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
 
 export default function FirstScreen() {
+  const [showSearch, setSearch] = useState(false);
   return (
     <View style={styles.container}>
       <StatusBar style='light' />
@@ -11,7 +20,34 @@ export default function FirstScreen() {
         source={require('../assets/images/tlo.jpg')}
         style={styles.imageBackground}
       />
-      <SafeAreaView style={styles.safeArea}></SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.searchContainer}>
+          <View
+            style={[
+              styles.searchBox,
+              {
+                backgroundColor: showSearch
+                  ? 'rgb(255, 255, 255)'
+                  : 'transparent',
+              },
+            ]}
+          >
+            {showSearch ? (
+              <TextInput
+                placeholder='Szukaj miasta'
+                placeholderTextColor={'lightgray'}
+                style={styles.input}
+              />
+            ) : null}
+            <TouchableOpacity
+              onPress={() => setSearch(!showSearch)}
+              style={styles.searchButton}
+            >
+              <MagnifyingGlassIcon size={25} color='black' />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
