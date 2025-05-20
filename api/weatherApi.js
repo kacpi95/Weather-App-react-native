415707296a0fffc6af6a7572e33cc86549cc67fe
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-05-20 13:30:34 +0200

    Add apiCall function

--- api/weatherApi.js
+++ api/weatherApi.js
@@ -5,3 +5,17 @@ const forecastEndpoint = (params) =>
   `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
 const locationEndpoint = (params) =>
   `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
+
+const apiCall = async (endpoint) => {
+  const options = {
+    method: 'GET',
+    url: endpoint,
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (err) {
+    console.log('error:', err);
+    return null;
+  }
+};
